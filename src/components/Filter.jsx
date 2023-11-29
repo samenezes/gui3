45dc5db42f92b97875e4c883bbbbc6446635271b
samenezes <61150892+samenezes@users.noreply.github.com>
2023-11-29 11:23:44 -0300

    Add files via upload

---
+++ src/components/Filter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Filter = ({filter, setFilter, setSort}) => {
+  return (
+    <div className='filter'>
+        <h2>Filtrar:</h2>
+        <div className='filter-options'>
+            <div>
+                <p>Status</p>
+                <select value={filter} onChange={(e) => setFilter(e.target.value)}> 
+                    <option value="All">Todas</option>
+                    <option value="Completed">Completas</option>
+                    <option value="Incomplete">Incompletas</option>
+                </select>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Filter
